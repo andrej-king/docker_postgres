c51d71d5c03819a60421ff11d4f32f7fe70bf7fa
Andrej Pedchenko <andrej.cdo@gmail.com>
2023-03-07 22:16:37 +0200

    Added pgadmin service

---
+++ pgadmin/init.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+
+## pgadmin init script
+
+set -e
+set -u
+
+#####> pgadmin file #####
+# Create pgpass file
+echo "$DB_HOST:$DB_PORT:$APP_DB_NAME:$APP_DB_USER:$APP_DB_PASS" >> /var/lib/pgadmin/pgpass
+
+# Set permissions for pgpass file
+chmod 600 /var/lib/pgadmin/pgpass
+chown pgadmin:root /var/lib/pgadmin/pgpass
+#####< pgadmin file #####
+
+#####> main postgres pgadmin file #####
+# Create postgres-pgpass file
+echo "$DB_HOST:$DB_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> /var/lib/pgadmin/postgres-pgpass
+
+# Set permissions for postgres-pgpass file
+chmod 600 /var/lib/pgadmin/postgres-pgpass
+chown pgadmin:root /var/lib/pgadmin/postgres-pgpass
+#####< main postgres pgadmin file #####
+
+
+#####> servers.json file #####
+# Create servers.json
+tee /pgadmin4/servers.json >/dev/null <<EOF
+{
+    "Servers": {
+        "1": {
+            "Name": "Local $APP_DB_USER",
+            "Group": "Servers",
+            "Host": "$DB_HOST",
+            "Port": $DB_PORT,
+            "MaintenanceDB": "$APP_DB_NAME",
+            "Username": "$APP_DB_USER",
+            "SSLMode": "prefer",
+            "PassFile": "/var/lib/pgadmin/pgpass"
+        },
+        "2": {
+            "Name": "Local $POSTGRES_USER",
+            "Group": "Servers",
+            "Host": "$DB_HOST",
+            "Port": $DB_PORT,
+            "MaintenanceDB": "$POSTGRES_DB",
+            "Username": "$POSTGRES_USER",
+            "SSLMode": "prefer",
+            "PassFile": "/var/lib/pgadmin/postgres-pgpass"
+        }
+    }
+}
+EOF
+
+# Set permissions for servers.json file
+chown pgadmin:root /pgadmin4/servers.json
+#####< servers.json file #####
+
+
+exec /entrypoint.sh "$@"
